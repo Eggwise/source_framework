117230c0c53f31c3e35d30ab8742c72603f74aaf
eggwise <dev@eggwise.com>
2016-10-21 09:13:22 +0200

    parameter info

--- generators/base.py
+++ generators/base.py
@@ -4,14 +4,21 @@ from ..models.components import SourceFile, Folder, SourceComponent
 from ..models.indexer import Indexed
 
 from .. import framework_manager as fm
-
+import logging
 
 def root_config(output_path='HERE', output_folder=None):
+    if output_folder is not None and output_path != 'HERE':
+        logging.info(
+            'Generate base indice\n====>>\nprovided an output path AND folder.\nThe output path overides the folder parameter.\nusing the folder at path: {0}\n<<======'.format(
+                output_path))
+
+        output_folder = Folder.from_path(output_path)
+
+
     if output_folder is None:
         if output_path == 'HERE':
-
             caller_frame = inspect.getouterframes(inspect.currentframe())[1]
-            output_folder = SourceComponent.from_frame(caller_frame).folder  # type: Folder
+            output_folder = SourceComponent.from_frame(caller_frame).folder # type: Folder
         else:
             output_folder = Folder.from_path(output_path)
 
@@ -23,13 +30,21 @@ def root_config(output_path='HERE', output_folder=None):
 
 def base_indices(output_path = 'HERE', output_folder=None):
 
+    if output_folder is not None and output_path != 'HERE':
+        logging.info(
+            'Generate base indice\n====>>\nprovided an output path AND folder.\nThe output path overides the folder parameter.\nusing the folder at path: {0}\n<<======'.format(
+                output_path))
+
+        output_folder = Folder.from_path(output_path)
+
     if output_folder is None:
         if output_path == 'HERE':
-
             caller_frame = inspect.getouterframes(inspect.currentframe())[1]
             output_folder = SourceComponent.from_frame(caller_frame).folder # type: Folder
         else:
             output_folder = Folder.from_path(output_path)
+
+
 
 
     config = fm.config # type: Folder
